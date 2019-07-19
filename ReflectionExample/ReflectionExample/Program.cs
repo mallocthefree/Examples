1a42cce9ba57d54867f5220d0dc9e5ceb214ffe3
Jeremy Snyder <51911349+mallocthefree@users.noreply.github.com>
2019-07-19 08:29:44 -0500

    Added comments
    Added some comments for a better understanding of what is written

--- ReflectionExample/ReflectionExample/Program.cs
+++ ReflectionExample/ReflectionExample/Program.cs
@@ -7,11 +7,16 @@ namespace ReflectionExample
     {
         private static void Main(string[] args)
         {
+            // Load all classes and instantiate each correctly defined class in the config file
             ExampleFactory.LoadAllExamples();
+
+            // Run Startup() on all IExampleRoot defined class instantiations
             ExampleFactory.StartAllExamples();
 
+            // Wait 1 second
             Thread.Sleep(1000);
 
+            // Run Shutdown() on all IExampleRoot defined class instantiations
             ExampleFactory.ShutdownAllExamples();
         }
     }
--- ReflectionExample/RootLibrary/ExampleFactory.cs
+++ ReflectionExample/RootLibrary/ExampleFactory.cs
@@ -17,15 +17,19 @@ namespace RootLibrary
         /// </summary>
         public static void LoadAllExamples()
         {
+            // Get the values listed in the config file within the section labeled <examples>
             var exampleHandlers = ConfigExamples;
+
             if (exampleHandlers == null)
             {
+                // Failed to find the <examples> configuration block (or config file)
                 return;
             }
 
             foreach (string key in exampleHandlers.AllKeys)
             {
-                GetExamples(key);
+                // Force a pre-cache of all of the listed config items
+                GetExample(key);
             }
         }
 
@@ -34,24 +38,37 @@ namespace RootLibrary
         /// </summary>
         public static void StartAllExamples()
         {
+            // Load configurations and make sure we have a cached copy of each
             LoadAllExamples();
+
+            // Run Startup() on each successfully loaded and cached instantiation of IExampleRoot
             foreach (var example in ExampleDictionary.Values)
             {
                 example.Startup();
             }
         }
 
+        /// <summary>
+        /// Run the shutdown process for every dynamically loaded Example library
+        /// </summary>
         public static void ShutdownAllExamples()
         {
+            // Run Shutdown() on each of the loaded IExampleRoot instantiated classes
             foreach (var example in ExampleDictionary.Values)
             {
                 example.Shutdown();
             }
 
+            // Empty the cached instantiations
             ExampleDictionary.Clear();
         }
 
-        public static IExampleRoot GetExamples(string module)
+        /// <summary>
+        /// Get an Example by name
+        /// </summary>
+        /// <param name="module">name of the Example module to get, as identified by the key in the config file</param>
+        /// <returns>Instantiation of an IExampleRoot concrete class</returns>
+        public static IExampleRoot GetExample(string module)
         {
             var example = ConfigExamples;
             if (example == null)
@@ -59,31 +76,49 @@ namespace RootLibrary
                 return null;
             }
 
+            // Get the string definition of the class to look for
             string moduleType = example.Get(module);
+
+            // Get the class as a Type
             Type type = Type.GetType(moduleType);
-            return GetExamples(type);
+            return GetExample(type);
         }
 
-        public static IExampleRoot GetExamples(Type type)
+        /// <summary>
+        /// Get the example by the class type (eg, ExampleClass_1)
+        /// If it was not yet instantiated, then do so during this call and cache the result
+        /// </summary>
+        /// <param name="type">Type of class to retrieve from cached instantiations</param>
+        /// <returns>Instantiation of an IExampleRoot concrete class</returns>
+        public static IExampleRoot GetExample(Type type)
         {
             if (type == null)
             {
                 return null;
             }
 
+            // Lock the dictionary for multi-threading to make sure we don't add while reading
             lock (ExampleDictionary)
             {
+                // Check cache to see if we already instantiated one
                 if (ExampleDictionary.ContainsKey(type))
                 {
+                    // Existence in cache found, so retrieve it
                     return ExampleDictionary[type];
                 }
 
+                // Instantiate this class as type
                 if (!(Activator.CreateInstance(type) is IExampleRoot example))
                 {
+                    // The class was not able to correctly be identified by code or library.
+                    // The configuration should be validated and the folder checked to ensure
+                    // the file exists as identified in the config file
                     return null;
                 }
 
                 Console.WriteLine($"Example [{type}] loaded");
+
+                // New instantiation successfully created, so cache it for future calls
                 ExampleDictionary.Add(type, example);
                 return example;
             }
